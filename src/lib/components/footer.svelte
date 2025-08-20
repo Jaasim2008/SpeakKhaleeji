0c5105bf7a4cd05a78295502487e6eaf74bb6c54
Jaasim2008 <jaasim2008@gmail.com>
2025-08-21 00:38:47 +0400

    footer text white

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,5 @@
 <footer class="bg-secondary py-1 border-t border-t-black">
-    <div class="max-w-7xl mx-auto px-4 text-center text-xs md:text-base">
+    <div class="max-w-7xl mx-auto px-4 text-center text-xs md:text-base text-white">
         <p>&copy; 2025 SpeakKhaleeji</p>
     </div>
 </footer>
