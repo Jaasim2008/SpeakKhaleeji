7e0c9f1680d9e3a0dcf8eebc6c9dc928acd0136e
Jaasim2008 <jaasim2008@gmail.com>
2025-08-21 00:45:14 +0400

    order by lessons to latest first

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,7 +4,8 @@ import { supabase } from '$lib/supabaseClient';
 export const load: LayoutServerLoad = async () => {
     let { data: lessons, error } = await supabase
         .from('lessons')
-        .select('*');
+        .select('*')
+        .order('created_at', {ascending: false});
         
     if (error) {
         console.error('Error loading lessons:', error.message);
